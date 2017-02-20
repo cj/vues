42ca31ae0b08711fcc7372081c5a7fd7ff1958d7
cj <cjlazell@gmail.com>
2017-02-20 01:33:37 -0600

    webpack

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 require('babel-register')({
   presets: ['vue-app', 'es2015'],
-  plugins: ['transform-runtime'],
+  plugins: ['transform-runtime', 'add-module-exports'],
   only: /vues\/lib\/.*.js$/
 })
 
-const server = require('./lib/server').default
-const config = require('./lib/config').default
+process.env.NODE_ENV = process.env.NODE_ENV || 'development'
+
+const server = require('./lib/server')
+const config = require('./lib/config')
 const utils  = require('./lib/utils')
 
 module.exports = { server, config, utils }
