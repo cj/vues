c05dfdadb34541f02ab4fc36c9662e6a3e77fbfd
cj <cjlazell@gmail.com>
2017-02-23 03:36:04 -0600

    core-js added for polyfill, objectToString, tests and template updates

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 require('babel-register')({
   presets: ['vue-app', 'es2015'],
-  plugins: ['add-module-exports'],
-  only: /vues\/lib\/.*.js$/
+  plugins: ['transform-runtime', 'add-module-exports'],
+  only: /(vues\/lib\/.*.js|vues.config.js)$/
 })
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
 const server = require('./lib/server')
-const config = require('./lib/config')
+const config = require('./lib/config').default
 const utils  = require('./lib/utils')
 
 module.exports = { server, config, utils }
