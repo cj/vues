d5e4485c270d9c6a19e99b0ca3c621cd368cc190
cj <cjlazell@gmail.com>
2017-02-26 15:07:24 -0600

    tests, cleanup and app hooks

--- lib/templates/App.vue
+++ lib/templates/App.vue
@@ -3,3 +3,18 @@
     <router-view></router-view>
   </div>
 </template>
+
+<script>
+  export default {
+    <% hooks.forEach(hook => { %>
+      {{ hook.name }} () {
+        {{ hook.callback }}
+        {{ hook.name }}.call(this)
+      },
+    <% }) %>
+  }
+</script>
+
+<% styles.forEach((style) => { %>
+  <style src='{{ style.src }}' lang='{{ style.lang || "css" }}'><style>
+<% }) %>
